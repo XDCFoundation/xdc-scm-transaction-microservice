f4d842fb52802ea82e0704c3e60d0d0fdb648faf
deepali <deepali@leewayhertz.com>
2022-01-21 15:30:26 +0530

    add and get network

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@
 import * as ValidationManger from "../middleware/validation";
 import { stringConstants } from "../app/common/constants";
 import TransactionModule from "../app/modules/transaction";
+import NetworkModule from "../app/modules/network/index";
 module.exports = (app) => {
   app.get("/", (req, res) => res.send(stringConstants.SERVICE_STATUS_HTML));
   app.post("/transaction", new TransactionModule().addTransaction);
   app.post("/get-transaction", new TransactionModule().getTransactionList);
+  //networks route
+  app.post("/add-network", new NetworkModule().addNetwork);
+  app.post("/get-network", new NetworkModule().getNetworksList);
 
   /**
    * route definition
