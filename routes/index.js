94a19ed13320f17c2ec1bac5b0197fc169f2da39
kajal_KajalBobal <kajal@leewayhertz.com>
2022-02-18 19:15:08 +0530

    analytics apis

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,11 @@ module.exports = (app) => {
   app.get("/", (req, res) => res.send(stringConstants.SERVICE_STATUS_HTML));
   app.post("/transaction", new TransactionModule().addTransaction);
   app.post("/get-transaction", new TransactionModule().getTransactionList);
+  app.post("/get-transaction-analytics",ValidationManger.validateAnalyticsRequest, new TransactionModule().getTransactionAnalytics);
+  app.post("/get-gas-used-analytics", ValidationManger.validateAnalyticsRequest, new TransactionModule().getGasUsedAnalytics);
+  app.post("/get-active-users-analytics",ValidationManger.validateAnalyticsRequest, new TransactionModule().getActiveUsersAnalytics);
+
+
   //networks route
   app.post("/add-network", new NetworkModule().addNetwork);
   app.post("/get-network", new NetworkModule().getNetworksList);
