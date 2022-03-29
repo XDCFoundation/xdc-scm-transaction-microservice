f300f9c0838fd441ea4855d73b004c11253b58be
kajal_KajalBobal <kajal@leewayhertz.com>
2022-03-29 18:51:21 +0530

    authentication integerattion

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ import TransactionModule from "../app/modules/transaction";
 import NetworkModule from "../app/modules/network/index";
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../config/swagger.json';
-import {authenticate} from "../middleware/authentication";
+import { authenticate } from "../middleware/authentication";
 
 module.exports = (app) => {
   app.get("/", (req, res) => res.send(stringConstants.SERVICE_STATUS_HTML));
@@ -18,13 +18,13 @@ module.exports = (app) => {
   app.use('/swagger-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.post("/transaction", new TransactionModule().addTransaction);
-  app.post("/get-transaction-list", new TransactionModule().getTransactionList);
-  app.post("/get-transaction",ValidationManger.validateTransactionHash, new TransactionModule().getTransactionByHash);
-  app.post("/get-transaction-analytics",ValidationManger.validateAnalyticsRequest, new TransactionModule().getTransactionAnalytics);
-  app.post("/get-gas-used-analytics", ValidationManger.validateAnalyticsRequest, new TransactionModule().getGasUsedAnalytics);
-  app.post("/get-active-users-analytics",ValidationManger.validateAnalyticsRequest, new TransactionModule().getActiveUsersAnalytics);
-  app.post("/get-top-callers",ValidationManger.validateAnalyticsRequest, new TransactionModule().getTopCallers);
-  app.post("/get-top-function-calls",ValidationManger.validateAnalyticsRequest, new TransactionModule().getTopFunctionCalls);
+  app.post("/get-transaction-list", authenticate, new TransactionModule().getTransactionList);
+  app.post("/get-transaction", authenticate, ValidationManger.validateTransactionHash, new TransactionModule().getTransactionByHash);
+  app.post("/get-transaction-analytics", authenticate, ValidationManger.validateAnalyticsRequest, new TransactionModule().getTransactionAnalytics);
+  app.post("/get-gas-used-analytics", authenticate, ValidationManger.validateAnalyticsRequest, new TransactionModule().getGasUsedAnalytics);
+  app.post("/get-active-users-analytics", authenticate, ValidationManger.validateAnalyticsRequest, new TransactionModule().getActiveUsersAnalytics);
+  app.post("/get-top-callers", authenticate, ValidationManger.validateAnalyticsRequest, new TransactionModule().getTopCallers);
+  app.post("/get-top-function-calls", authenticate, ValidationManger.validateAnalyticsRequest, new TransactionModule().getTopFunctionCalls);
   app.post("/get-gasprice-in-USD", new TransactionModule().getTGasPriceInUSD);
 
 
